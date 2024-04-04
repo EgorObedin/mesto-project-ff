c472d239191568401048eb1ab2640801e5f43ce7
Egor Obedin <obedin.egor@gmail.com>
2024-04-04 23:09:59 +0300

    Комит_1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-
+const placeslist = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function createCardElm(cardData, deleteCallBack) {
+  const CardElm = cardTemplate.querySelector(".card").cloneNode(true);
+  const ImageElm = CardElm.querySelector(".card__image");
+  const TitleElm = CardElm.querySelector(".card__title");
 
+  ImageElm.src = cardData.link;
+  ImageElm.alt = cardData.name;
+  TitleElm.textContent = cardData.name;
+
+  const deleteButton = CardElm.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => {
+    deleteCallBack(CardElm);
+  });
+  return CardElm;
+}
 // @todo: Функция удаления карточки
-
+function deleteCard(CardElm) {
+  CardElm.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardData) => {
+  placeslist.append(createCardElm(cardData, deleteCard));
+});
