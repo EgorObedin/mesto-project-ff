5359b24eb332d938a82ff98487e4899aa3d33c0d
Egor Obedin <obedin.egor@gmail.com>
2024-04-05 10:40:05 +0300

    Комит_2

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,28 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
-const placeslist = document.querySelector(".places__list");
+const placesList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
 function createCardElm(cardData, deleteCallBack) {
-  const CardElm = cardTemplate.querySelector(".card").cloneNode(true);
-  const ImageElm = CardElm.querySelector(".card__image");
-  const TitleElm = CardElm.querySelector(".card__title");
+  const cardElm = cardTemplate.querySelector(".card").cloneNode(true);
+  const ImageElm = cardElm.querySelector(".card__image");
+  const TitleElm = cardElm.querySelector(".card__title");
 
   ImageElm.src = cardData.link;
   ImageElm.alt = cardData.name;
   TitleElm.textContent = cardData.name;
 
-  const deleteButton = CardElm.querySelector(".card__delete-button");
+  const deleteButton = cardElm.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", () => {
-    deleteCallBack(CardElm);
+    deleteCallBack(cardElm);
   });
-  return CardElm;
+  return cardElm;
 }
 // @todo: Функция удаления карточки
-function deleteCard(CardElm) {
-  CardElm.remove();
+function deleteCard(cardElm) {
+  cardElm.remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach((cardData) => {
-  placeslist.append(createCardElm(cardData, deleteCard));
+  placesList.append(createCardElm(cardData, deleteCard));
 });
